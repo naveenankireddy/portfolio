90af0cc488df998e6ffd3c1742265b336919f443
naveenankireddy <naveenkumarreddyankireddy@gmail.com>
2020-10-29 00:02:57 +0530

    changes in pages

--- pages/portfolio.jsx
+++ pages/portfolio.jsx
@@ -30,7 +30,7 @@ function Portfolio() {
       <p className="description">Helping-Hands is made with pure HTML & CSS. 
 </p>
       <a href="https://github.com/AltCampus/helping-hands
-" class="cta">GitHub Link <span class="ti-angle-right"></span></a>
+" class="cta">Link <span class="ti-angle-right"></span></a>
     </div>
 
     <div class="service service1">
@@ -39,7 +39,7 @@ function Portfolio() {
       <p className="description">Book-Shelf  is made with Node Js,Express Js,MongoDB.
 </p>
       <a href="https://github.com/naveenankireddy/Book_Shelf
-" class="cta">GitHub Link <span class="ti-angle-right"></span></a>
+" class="cta">Link <span class="ti-angle-right"></span></a>
     </div>
 
     <div class="service service1">
@@ -50,7 +50,7 @@ function Portfolio() {
 </p>
       <a href="https://github.com/naveenankireddy/conduit-react-redux
 
-" class="cta">GitHub Link<span class="ti-angle-right"></span></a>
+" class="cta">Link<span class="ti-angle-right"></span></a>
     </div>
 	<div class="service service1">
       <i class="ti-money"></i>
@@ -58,7 +58,7 @@ function Portfolio() {
       <p className="description">Coin collector game  is made with pure Vanilla Js.
 </p>
       <a href="https://github.com/naveenankireddy/coin-collector-game
-" class="cta">GitHub Link <span class="ti-angle-right"></span></a>
+" class="cta">Link <span class="ti-angle-right"></span></a>
     </div>
   </div>
 </section>
@@ -70,38 +70,38 @@ function Portfolio() {
   <div class="services-grid">
     <div class="service service1">
       <i class="ti-bar-chart"></i>
-      <h4>React</h4>
+      <h4>ReactJs</h4>
       <p className="description">Things to recall before touching React.
 </p>
       <a href="https://medium.com/@naveenkumarreddyankireddy/things-to-recall-in-javascript-before-entering-into-react-js-177b798322ec
-" class="cta">Medium Link <span class="ti-angle-right"></span></a>
+" class="cta">Link <span class="ti-angle-right"></span></a>
     </div>
 
     <div class="service service1">
       <i class="ti-light-bulb"></i>
-      <h4>Node Js</h4>
+      <h4>NodeJs</h4>
       <p className="description">I wrote this on Basic introduction about Node.js.
 </p>
       <a href="https://medium.com/@naveenkumarreddyankireddy/basic-intro-to-node-js-d9515a588b8a
-" class="cta">Medium Link <span class="ti-angle-right"></span></a>
+" class="cta">Link <span class="ti-angle-right"></span></a>
     </div>
 
     <div class="service service1">
       <i class="ti-money"></i>
-      <h4>JS</h4>
+      <h4>JavaScript</h4>
       <p className="description">I wrote this on Higher order functions in JS like map,filter etc...
 
 </p>
       <a href="https://medium.com/@naveenkumarreddyankireddy/javascript-higher-order-functions-map-foreach-filter-99e8e1513603
-" class="cta">Medium Link<span class="ti-angle-right"></span></a>
+" class="cta">Link<span class="ti-angle-right"></span></a>
     </div>
 	<div class="service service1">
       <i class="ti-money"></i>
-      <h4>JS</h4>
-      <p className="description">I wrote this on Detail explanation of Dom Manupulation.
+      <h4>HTML & CSS</h4>
+      <p className="description">I wrote this on detailed about Flex-Box.
 </p>
-      <a href="https://medium.com/@naveenkumarreddyankireddy/basic-explanation-on-dom-73ed11ea974d
-" class="cta">Medium Link <span class="ti-angle-right"></span></a>
+      <a href="https://medium.com/@naveenkumarreddyankireddy/flex-box-a8d4a65df671
+" class="cta">Link <span class="ti-angle-right"></span></a>
     </div>
   </div>
 
